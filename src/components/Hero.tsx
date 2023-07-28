ba3a45ffaa667d22ca0234c447717eb17af86cb3
frankooz <pobletefranco49@gmail.com>
2023-07-28 16:16:01 -0300

    a ver fix hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="flex items-center h-auto bg-blue-200">
-      <img src="/hero1.jpg" alt="" className="w-full sm:w-3/4 mx-auto" />
+      <img src="/hero1.JPG" alt="" className="w-full sm:w-3/4 mx-auto" />
     </div>
   );
 };
