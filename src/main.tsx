c2b4ca06675d72a43bddf71aaf1aa771654e93a2
frankooz <pobletefranco49@gmail.com>
2023-07-28 21:39:20 -0300

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={import.meta.env.DEV ? "/" : "/minga_web/"}>
+  <HashRouter basename={import.meta.env.DEV ? "/" : "/minga_web/"}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
